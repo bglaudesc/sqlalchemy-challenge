6792585563ce96cad97911fdbf1e95910cd545ac
Brian G <bglaudesc@yahoo.com>
2020-07-19 10:01:20 -0500

    update app

--- app.py
+++ app.py
@@ -41,15 +41,20 @@ def welcome():
         f"Home page<br/>"
         f"List all routes that are available:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/START DATE GOES HERE (YYYY-MM-DD)<br/>"
+        f"/api/v1.0/START DATE GOES HERE (YYYY-MM-DD) / END DATE GOES HERE (YYYY-MM-DD)<br/>"
+        
     )
 
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    Previous_year ='2016-08-23'
     Data_Precipitation_12_months = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= Previous_year).\
     order_by(Measurement.date).all()
 
     date_prec_dict = {}
@@ -57,6 +62,19 @@ def precipitation():
         date_prec_dict[tuple_[0]]=tuple_[1]
     session.close()
     return jsonify(date_prec_dict)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    list_of_stations = session.query(Station.station).\
+    order_by(Station.station).all()
+
+    stations_loop=[]
+    for tuple_ in list_of_stations:
+        stations_loop.append(tuple_[0])
+
+    return jsonify(stations_loop)
+
 
 @app.route("/api/v1.0/tobs")
 def tobs_temp():
@@ -71,13 +89,24 @@ def tobs_temp():
     session.close()
     return jsonify(tempature_obeservation)
 
-
 @app.route("/api/v1.0/<start>")
-def given_start(start):
-    return(start)
-    
-session.close()
-    return jsonify(tempature_obeservation)
+@app.route("/api/v1.0/<start>/<end>")
+def given_start(start,end=None):
+    if end==None:
+        start_route = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).all()[0]
+    else:
+        start_route = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).filter(Measurement.date <= end).all()[0]
+    start_dictionary = {
+        "min":start_route[0],
+        "avg":start_route[1],
+        "max":start_route[2],
+    }
+
+  
+    session.close()
+    return jsonify(start_dictionary)
 
 
 
